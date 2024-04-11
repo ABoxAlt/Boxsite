74d47736c53fe8c303df693286da660ec3b1518e
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-04-11 13:39:09 -0700

    First Project complete

--- games/First/First.js
+++ games/First/First.js
@@ -2,7 +2,55 @@
 const canvas = document.querySelector('#game');
 const ctx = canvas.getContext('2d');
 
+// game vars
+const boxDim = 25;
+let boxX = 0;
+let boxY = 0;
 
+window.addEventListener('keydown', move);
 
-ctx.fillStyle = '#1B512D';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+function paintBox() {
+  ctx.save();
+  ctx.fillStyle = 'black';
+  ctx.fillRect(boxX, boxY, boxDim, boxDim);
+  ctx.restore();
+}
+
+function paint() {
+  ctx.fillStyle = '#73E2A7';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  paintBox();
+}
+
+function tick() {
+  paint();
+}
+
+function move(keyboard) {
+  console.log(keyboard.key);
+  switch(keyboard.key) {
+    case 'w':
+      if (boxY > 0) {
+        boxY -= 3;
+      }
+      break;
+    case 's':
+      if (boxY < canvas.height - boxDim) {
+        boxY += 3;
+      }
+      break;
+    case 'a':
+      if (boxX > 0) {
+        boxX -= 3;
+      }
+      break;
+    case 'd':
+      if (boxX < canvas.width - boxDim) {
+        boxX += 3;
+      }
+      break;
+  }
+  paint();
+}
+
+paint();
