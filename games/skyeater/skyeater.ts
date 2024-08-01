7119b87c882eeb93f4d9df8085010379ca817128
ABoxAlt <150751474+ABoxAlt@users.noreply.github.com>
2024-08-01 13:19:37 -0700

    Progress On Skyeater

--- games/skyeater/skyeater.ts
+++ games/skyeater/skyeater.ts
@@ -1,45 +1,79 @@
-class textWriter {
+class TextWriter {
   constructor() {}
 
   public writeTitle(text:string, ctx:CanvasRenderingContext2D) {
 
   }
 
+  public testFunction(): Promise<Image> {
+    return new Promise((resolve, reject) => {
+      const testImage = new Image();
+      testImage.src = "./Fonts/Title.png";
+      testImage.onload = () => {
+        resolve(testImage);
+      };
+      testImage.onerror = (e) => { reject(e); }
+    });
+  }
   public writeText(text:string, fontStyle:string, size:number, x:number, y:number, ctx:CanvasRenderingContext2D) {
     const startX = x;
     for (const character of text) {
-      ctx.drawImage(this.characterFinder(character, fontStyle), x, y, size, size);
+      //ctx.drawImage(this.characterFinder(character, fontStyle), x, y, size, size);
       x += size * 1.2;
     }
   }
 
   private characterFinder(character:string, fontStyle:string) {
-    const returnImg = new Image();
-    returnImg.src = "./Fonts/Title/Title00.png";
+    const returnImg = new Image(20, 20) as HTMLImageElement;
+    let sx, sy, sw, sh = 0;
     if (fontStyle == "Title") {
+      returnImg.src = "./Fonts/Title.png";
+      sw = 500;
+      sh = 500;
+
       switch (character.toLowerCase()) {
-        case "a":
-          returnImg.src = "./Fonts/Title/Title00.png";
+        case 'a':
+          sx = 0;
+          sy = 0;
           break;
         default:
-          returnImg.src = "./Fonts/Title/Title00.png";
+          sx = 0;
+          sy = 0;
           break;
       }
     }
-    return returnImg;
+    return createImageBitmap(returnImg, sx, sy, sw, sh);
   }
 }
 
 class SkyEater {
-  textWriter = new textWriter();
+  textWriter = new TextWriter();
   
   constructor() {
     const canvas = document.getElementById('game') as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d');
-    ctx!.fillStyle = 'white';
-    ctx!.fillRect(0, 0, canvas.width, canvas.height);
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-    this.textWriter.writeText('a', 'Title', 100, 100, 100, ctx!);
+    this.loadResources(ctx);
+  }
+
+  async loadResourcesAsync(ctx: CanvasRenderingContext2D) {
+    try {
+    const [img1, img2] = await Promise.all([this.textWriter.testFunction(), this.textWriter.testFunction()]);
+    ctx.drawImage(img1, 0, 0, 500, 500, 0, 0, 100, 100);
+    ctx.drawImage(img2, 0, 0, 500, 500, 100, 100, 100, 100);
+    //this.textWriter.writeText('a', 'Title', 100, 100, 100, ctx);
+    } catch (e) {
+      console.error("WTF?", e);
+    }
+  }
+
+  loadResources(ctx: CanvasRenderingContext2D) {
+    Promise.all([this.textWriter.testFunction(), this.textWriter.testFunction()]).then(([img1, img2]) => {
+      ctx.drawImage(img1, 0, 0, 500, 500, 0, 0, 100, 100);
+      ctx.drawImage(img2, 0, 0, 500, 500, 100, 100, 100, 100);
+    }).catch((e) => {
+      console.error("WTF?", e);
+    });
   }
 
 }
--- output/games/skyeater/skyeater.js
+++ output/games/skyeater/skyeater.js
@@ -1,39 +1,72 @@
 (() => {
   // skyeater.ts
-  var textWriter = class {
+  var TextWriter = class {
     constructor() {
     }
     writeTitle(text, ctx) {
     }
+    testFunction() {
+      return new Promise((resolve, reject) => {
+        const testImage = new Image();
+        testImage.src = "./Fonts/Title.png";
+        testImage.onload = () => {
+          resolve(testImage);
+        };
+        testImage.onerror = (e) => {
+          reject(e);
+        };
+      });
+    }
     writeText(text, fontStyle, size, x, y, ctx) {
       const startX = x;
       for (const character of text) {
-        ctx.drawImage(this.characterFinder(character, fontStyle), x, y, size, size);
         x += size * 1.2;
       }
     }
     characterFinder(character, fontStyle) {
-      const returnImg = new Image();
-      returnImg.src = "./Fonts/Title/Title00.png";
+      const returnImg = new Image(20, 20);
+      let sx, sy, sw, sh = 0;
       if (fontStyle == "Title") {
+        returnImg.src = "./Fonts/Title.png";
+        sw = 500;
+        sh = 500;
         switch (character.toLowerCase()) {
           case "a":
-            returnImg.src = "./Fonts/Title/Title00.png";
+            sx = 0;
+            sy = 0;
+            break;
           default:
-            returnImg.src = "./Fonts/Title/Title00.png";
+            sx = 0;
+            sy = 0;
+            break;
         }
       }
-      return returnImg;
+      return createImageBitmap(returnImg, sx, sy, sw, sh);
     }
   };
   var SkyEater = class {
-    textWriter = new textWriter();
+    textWriter = new TextWriter();
     constructor() {
       const canvas = document.getElementById("game");
       const ctx = canvas.getContext("2d");
-      ctx.fillStyle = "white";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      this.textWriter.writeText("a", "Title", 100, 100, 100, ctx);
+      this.loadResources(ctx);
+    }
+    async loadResourcesAsync(ctx) {
+      try {
+        const [img1, img2] = await Promise.all([this.textWriter.testFunction(), this.textWriter.testFunction()]);
+        ctx.drawImage(img1, 0, 0, 500, 500, 0, 0, 100, 100);
+        ctx.drawImage(img2, 0, 0, 500, 500, 100, 100, 100, 100);
+      } catch (e) {
+        console.error("WTF?", e);
+      }
+    }
+    loadResources(ctx) {
+      Promise.all([this.textWriter.testFunction(), this.textWriter.testFunction()]).then(([img1, img2]) => {
+        ctx.drawImage(img1, 0, 0, 500, 500, 0, 0, 100, 100);
+        ctx.drawImage(img2, 0, 0, 500, 500, 100, 100, 100, 100);
+      }).catch((e) => {
+        console.error("WTF?", e);
+      });
     }
   };
   new SkyEater();
